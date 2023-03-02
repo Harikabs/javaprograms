2e22452077236697cb70bfd08c3319997707c229
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-03-02 12:13:14 +0530

    update

---
+++ day2/ASCII.java
@@ -0,0 +1,7 @@
+public class ASCII{
+public static void main(String args[]){
+int ch='a';
+System.out.println("ASCII value"+ch);
+}
+}
+
---
+++ day2/ObjCompare.java
@@ -0,0 +1,9 @@
+public class ObjComapare{
+public static void main(String args[]){
+Double x=new Double(34.6783);
+Long y=new Long(2344556);
+System.out.println("objects are not equal"+x.equals(y));
+System.out.println("objects  are equal"+y.equals(2344556));
+}
+}
+
