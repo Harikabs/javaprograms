770c71a0f294150f7a98e967c3c4db38b9f8ac51
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-03-02 11:23:08 +0530

    upadate

---
+++ day2/PrimeNumber.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class PrimeNumber{
+public static void main(String args[]){
+int flag=0;
+int n=sc.nextInt();
+int m=n/2;
+if(n==0||n==1){
+System.out.println("not a prime number");
+}
+else{
+for (int i=2;i<=m;i++){
+if(n%i==0){
+System.out.println("is not a prime number");
+flag=1;
+break;
+}
+}
+if(flag==0){
+System.out.println("it is  a prime number");
+}
+}
+}
+
+
