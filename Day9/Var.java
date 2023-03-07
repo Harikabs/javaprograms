64172d01f4243f71199f9a7862ccf2734a449ad7
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-03-07 17:11:02 +0530

    day9

--- Day9/Var.java
+++ Day9/Var.java
@@ -1,10 +1,10 @@
 public class Var{
 public static void main(String args[]){
-var x="nksjggjki";
-var a="34";
-var b="78";
-var c="a+b";
-var d="69.98";
+var x=nksjggjki;
+var a=34;
+var b=78;
+var c=a+b;
+var d=69.98;
 System.out.println(" integer:"+ a);
 System.out.println("string  "+x);
 System.out.println(" addition "+c);
