f742eee53b5ebe275854ac9aedd2751c8902158f
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-03-07 13:42:34 +0530

    commit

--- Day8/Butterfly.java
+++ Day8/Butterfly.java
@@ -1,16 +1,20 @@
 //program to print numer butterfly
+import java.util.*;
 public class Butterfly{
 public static void main(String args[]){
-int n=9;
+Scanner sc=new Scanner(System.in);
+System.out.print(" enter the number:");
+int n=sc.nextInt();
 for(int i=1;i<=n;i++){
-for(int j=n;j>=n-i+1;j--)
+for(int j=n;j>=n-i+1;j--){
+if(j==1)
+break;
 System.out.print(j);
+}
 for(int k=1;k<=n-i;k++)
 System.out.print(" ");
 for(int k=1;k<=n-i-1;k++)
 System.out.print(" ");
-//for(int j=n;j<=i;j++)
-//System.out.print(j);
 for(int j=n-i+1;j<=n;j++)
 System.out.print(j);
 System.out.println();
