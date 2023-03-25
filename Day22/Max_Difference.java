0eb5027bea0dd0f5cffe3fc5a8703cb53f0cf8d9
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-03-25 16:38:50 +0530

    array

--- Day22/Max_Difference.java
+++
@@ -1,8 +0,0 @@
-//program for finding the maximum difference between two elements starting from the second least element
-public class Max_Difference{
-public static void main(String args[]) {
-int a[]={7,8,8,9,6,5,4,3};
-int max=a[0];
-for(int i=0;i<n;++){
-if(a[i]>
-
