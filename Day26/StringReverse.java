56b4ade78a44c642cfd66e9d4a0ca2478ceb967c
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-03-25 12:19:41 +0530

    strings

---
+++ Day26/StringReverse.java
@@ -0,0 +1,9 @@
+public class StringReverse{
+public static void main(String args[]){
+String str=" i love programing soo much";
+int x=str.length();
+for(int i=x-1;i>=0;i--){
+System.out.print(str.charAt(i));
+}
+}
+}
---
+++ Day26/Word_Reverse.java
@@ -0,0 +1,14 @@
+public class Word_Reverse{
+public static void main(String args[]){
+String str=" i love programing soo much";
+int x=str.length();
+
+for(int i=x-1;i>=0;i--){
+if(str.charAt(i)==' '){
+for(int j=i+1;j<x;j++)
+System.out.print(str.charAt(j));
+x=i;
+}
+}
+}
+}
