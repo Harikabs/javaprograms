aa20371e318c0dc496ef1a4628184238b505757e
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-03-26 22:37:35 +0530

    strings

---
+++ Day27/Split_String.java
@@ -0,0 +1,11 @@
+//program for using the split screen
+import java.util.Arrays;
+public class Split_String{
+public static void main(String args[]){
+String str="hello harika how are you";
+String[] x=str.split("h",7);
+for(var a:x)
+System.out.println(a);
+}
+}
+
