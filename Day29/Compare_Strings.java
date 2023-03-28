5c6ea260c5eb30f75ddb74b62e6a90098d5dbc24
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-03-28 22:52:50 +0530

    arrays

---
+++ Day29/Compare_Strings.java
@@ -0,0 +1,9 @@
+//program for comparing the two strings
+public class Compare_Strings{
+public static void main(String args[]){
+String str1="harikaworld";
+String str2="hellomyworld";
+int x=str1.compareTo(str2);
+System.out.println(x);
+}
+}
