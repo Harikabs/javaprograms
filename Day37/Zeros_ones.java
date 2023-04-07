2562741515c9e8361e717bbc1269f04861ea0e05
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-04-07 11:26:56 +0530

    arrays

--- Day37/Zeros_ones.java
+++ Day37/Zeros_ones.java
@@ -4,9 +4,8 @@ public class Zeros_ones{
 public static void main(String args[]){
 int arr[]={0,1,0,1,0,1,0};
 int sum=0;
-for (int i=0;i<arr.length;i++){
+for (int i=0;i<arr.length;i++)
 sum+=arr[i];
-}
 int zeros=arr.length-sum;
 int one=sum;
 int i;
@@ -14,7 +13,7 @@ for(i=0;i<zeros;i++){
 arr[i]=0;
 }
 while(i<arr.length){
-arr[i]=1;
+arr[i++]=1;
 }
 System.out.print(Arrays.toString(arr));
 }
