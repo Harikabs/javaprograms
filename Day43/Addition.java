2d25688c6fa60d1b463910073046810fdffc67c7
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-04-13 22:51:59 +0530

    packages

---
+++ Day43/Addition.java
@@ -0,0 +1,17 @@
+//import Add.AddSecond;
+
+public class Addition{
+    public static void main(String[] args) {
+    Add.AddSecond a=new Add().new AddSecond();
+    a.add();
+    }
+}
+class Add{
+    int x=9;
+    class AddSecond{
+        int y=5;
+          void add(){
+            System.out.println(x+y);
+        }
+    }
+}
