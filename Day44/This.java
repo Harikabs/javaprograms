f8f3c6658cce4a909e6d2e5628d99409fd2d93fd
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-04-14 22:13:09 +0530

    update

---
+++ Day44/This.java
@@ -0,0 +1,16 @@
+public class This {
+    public static void main(String[] args) {
+       sum(7); 
+    }
+    
+    int x;
+    static void sum(int x){
+        this.x=x;
+        System.out.println("the sum");
+    }
+}
+    
+
+
+
+
