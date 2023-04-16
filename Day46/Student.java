cc3fcb690ae6c43bc9edacf5f3e7495a1927ad9f
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-04-16 22:47:31 +0530

    update

---
+++ Day46/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    public static void main(String[] args) {
+        System.out.println(Details.s);
+        Details.function();
+    }
+    
+}
+ class Details{
+     static String s="harika";
+     static void function(){
+
+        System.out.println("the function is about student");
+     }
+
+
+}
