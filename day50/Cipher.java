0e51c1a8408a6a575cba1017212351f7629c57c1
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-04-21 22:58:31 +0530

    update

---
+++ day50/Cipher.java
@@ -0,0 +1,28 @@
+public class Cipher {
+   static char arr[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+    public static void main(String[] args) {
+
+    String cipher="olssv dvysk";
+
+    decode(cipher);
+    }
+    static void decode(String str){
+         String cipher="";
+        for(int i=1;i<=25;i++){
+            for(int j=0;j<str.length();j++){
+                if(!(str.charAt(j)== ' ')){
+                    int ind=str.charAt(j)-97-i;
+                    int curNow=ind%26;
+                    if(curNow<0)
+                    curNow+=26;
+                    System.out.print(arr[curNow]);
+                }
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
