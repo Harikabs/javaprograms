a2231e218a496c71439a5e30b2fd1a351b3a272f
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-04-28 23:42:54 +0530

    update

---
+++ Day56/Program.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+public class Program {
+    public static void main(String[] args) {
+        Integer a = 5;
+        Integer b = Integer.parseInt(a);
+        System.out.println(b);
+    }
+
+}
