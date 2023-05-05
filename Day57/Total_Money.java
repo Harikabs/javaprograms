b3595bbcd7792a0f94d36f453708f00853667d50
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-05-05 23:53:51 +0530

    update

---
+++ Day57/Total_Money.java
@@ -0,0 +1,13 @@
+import java.util.*;
+    public class Total_Money {
+        public static void main (String[] args) 
+        {
+            // your code goes here
+            Scanner hh =  new Scanner(System.in);
+                int x = hh.nextInt();
+                int y = hh.nextInt();
+                System.out.println(x*10+90*y);
+                hh.close();
+            }
+        }
+    
