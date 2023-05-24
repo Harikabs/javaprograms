2b7ec1cd991472e41f396e42d3ec71a87126c54b
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-05-24 21:09:14 +0530

    updater

---
+++ simpleprograms/Bus_seatnumber.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Bus_seatnumber {
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner hh = new Scanner(System.in);
+		
+		
+		int n = hh.nextInt();
+		if(n<=10)
+		    System.out.println("lower double");
+		else if(n<=15)
+		    System.out.println("lower single");
+		else if(n<=25)
+		    System.out.println("upper double");
+		else
+		    System.out.println("upper single");
+		
+		
+		
+        hh.close();
+    }
+	}
+
+
