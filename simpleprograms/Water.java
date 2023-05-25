468600442ad255094fcc1e1589a88d0e17a35d88
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-05-25 22:03:21 +0530

    update

---
+++ simpleprograms/Water.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Water {
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    System.out.println(x/(y*2));
+		    sc.close();
+		}
+	}
+
+
