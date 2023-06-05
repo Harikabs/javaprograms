32d28c193c60aee859513c9414d0002374c1077c
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-06-05 22:19:40 +0530

    update

---
+++ simpleprograms/Bullet.java
@@ -0,0 +1,20 @@
+import java.util.*;
+/* Name of the class has to be "Main" only if the class is public. */
+public class Bullet {
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner hh = new Scanner(System.in);
+		    int x = hh.nextInt();
+		    int y = hh.nextInt();
+		    int z = hh.nextInt();
+		    if((z-(y/x))<=0)
+		        System.out.println(0);
+		    else
+		        System.out.println(z-(y/x));
+                hh.close();
+		}
+
+	}
+
+
