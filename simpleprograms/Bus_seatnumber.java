8d8b042acef83bf1aeef65ec3749d64223a16749
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-06-16 22:43:39 +0530

    update

--- simpleprograms/Bus_seatnumber.java
+++ simpleprograms/Bus_seatnumber.java
@@ -1,8 +1,5 @@
 import java.util.*;
 import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 public class Bus_seatnumber {
 
 	public static void main (String[] args) throws java.lang.Exception
