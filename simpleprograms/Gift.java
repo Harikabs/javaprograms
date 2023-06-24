d44181626625b0254a222e87ed24e638f3130e44
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-06-24 20:42:13 +0530

    Update

--- simpleprograms/Gift.java
+++ simpleprograms/Gift.java
@@ -3,9 +3,6 @@ import java.lang.*;
 import java.io.*;
 
 public class Gift {
-
-
-/* Name of the class has to be "Main" only if the class is public. */
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
