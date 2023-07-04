422b4928fdea2e9376d491c6ce55b41f30634ddf
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-07-04 23:32:41 +0530

    Update

--- simpleprograms/AC.java
+++ simpleprograms/AC.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
+
 public class AC {
 
 	public static void main (String[] args) throws java.lang.Exception
