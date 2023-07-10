7cc05e77c76dd87e92ada32af3551d6334bf8e40
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-07-10 21:50:15 +0530

    Update

--- simpleprograms/Water.java
+++ simpleprograms/Water.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Water {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		
 		Scanner sc=new Scanner(System.in);
 		
 		    int x=sc.nextInt();
