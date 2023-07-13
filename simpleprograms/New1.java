ace1b0068841d810e5a76ab6d3417c5ea8aed3ea
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-07-13 23:53:28 +0530

    Update

--- simpleprograms/New1.java
+++ simpleprograms/New1.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class New1 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		
 		Scanner hh = new Scanner(System.in);
 		int t = hh.nextInt();
 		for(int i=0;i<t;i++){
