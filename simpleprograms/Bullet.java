e2da5ad5fb9e25350a6ec26713df84441a077c7a
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-07-17 21:25:04 +0530

    Update

--- simpleprograms/Bullet.java
+++ simpleprograms/Bullet.java
@@ -1,5 +1,4 @@
 import java.util.*;
-/* Name of the class has to be "Main" only if the class is public. */
 public class Bullet {
 	public static void main (String[] args) throws java.lang.Exception
 	{
