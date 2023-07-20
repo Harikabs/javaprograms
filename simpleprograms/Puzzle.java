f1ba90ebd6218b00c213f67cde48dc15c603c6d0
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-07-20 23:06:39 +0530

    update

---
+++ simpleprograms/Puzzle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Puzzle
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner hh = new Scanner(System.in);
+		int n = hh.nextInt();
+		if(n>=6 && n<=8)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
