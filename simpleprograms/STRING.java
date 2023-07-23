1ab6574358fcf22e36840f51ccce4ab8bd1f6b7e
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-07-23 22:58:22 +0530

    Update

--- simpleprograms/STRING.java
+++ simpleprograms/STRING.java
@@ -2,8 +2,6 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 public class STRING {
 
 	public static void main (String[] args) throws java.lang.Exception
