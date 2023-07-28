cc5de4a6815c39894ea0fdfc642c8c0c046290b5
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-07-28 22:53:42 +0530

    Update

--- simpleprograms/Add.java
+++ simpleprograms/Add.java
@@ -1,8 +1,7 @@
 public class Add {
     public static void main(String[] args) {
         int  x = 6;
-        int  y = 7;
-        // addition of two variables 
+        int  y = 7; 
         System.out.println(x+y);
     }
 }
