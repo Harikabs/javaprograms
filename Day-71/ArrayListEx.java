41876d7bbec62f1ce418795579698aaf6fa8d7e1
Harikabs <120558174+Harikabs@users.noreply.github.com>
2023-08-17 21:24:49 +0530

    Update

--- Day-71/ArrayListEx.java
+++ Day-71/ArrayListEx.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class ArrayListEx {   
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(110);
+        arr.add(10);
         arr.add(20);
         arr.add(30);
         arr.add(40);
